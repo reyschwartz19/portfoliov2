5ae05891ce737a7678e83a883c2ff28880718a1d
rey <njongremy30@gmail.com>
2025-12-24 21:24:23 +0100

    layout,content and links

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Skills from './components/skills';
 import Projects from './components/projects';
 import test1Img from './assets/test1.jpg';
+import animeProjectImg from './assets/anime-project.png';
 
 function App() {
   
@@ -18,7 +19,7 @@ function App() {
 
   return (
    <main className='bg-primary text-secondary pb-10 pt-20 sm:pt-24 overflow-x-hidden'>
-     <header className="fixed top-0 left-0 z-50 flex items-center justify-between w-full px-5 py-4 border-b border-gray-300 bg-primary md:px-40">
+     <header className="fixed top-0 left-0 z-50 flex items-center justify-between w-full px-5 py-4 border-b border-gray-300 bg-primary/90 md:px-40">
   <span className="font-heading text-3xl">nrn.</span>
 
   <button onClick={toggleMenu} className="sm:hidden">
@@ -27,22 +28,23 @@ function App() {
 
   <nav
     className={`
-      absolute top-full left-0 w-full bg-primary
+      absolute top-full left-0 w-full bg-primary/90
       flex flex-col gap-4 px-5 py-4 max-h-[60vh] overflow-auto
       ${open ? 'block' : 'hidden'}
       sm:static sm:flex sm:flex-row sm:gap-5 sm:p-0 sm:w-auto
     `}
   >
-    <span className="font-primary">Home</span>
-    <span className="font-primary">Skills</span>
-    <span className="font-primary">Projects</span>
-    <span className="font-primary">Contact</span>
+    <a className="font-primary hover:cursor-pointer" href='#home'>Home</a>
+    <a className="font-primary hover:cursor-pointer" href='#skills'>Skills</a>
+    <a className="font-primary hover:cursor-pointer" href='#projects'>Projects</a>
+    <a className="font-primary hover:cursor-pointer" href='#contact'>Contact</a>
   </nav>
 </header>
 
     <section 
     className='flex min-h-screen flex-col items-center text-center px-10 justify-center font-primary text-secondary
-         sm:px-40'>
+         sm:px-40'
+    id='home'>
       <p className='font-heading text-4xl mb-3 text-black'>Njong Remy Nga'i</p>
       <p className='text-2xl mb-5'>Front End Developer</p>
       <p>Driven by problem solving and clarity of execution.
@@ -51,17 +53,26 @@ function App() {
         and visual balance. Dedicated to building digital experiences that feel purposeful, polished, and reliable.
       </p>
       <div className='flex gap-4 mt-7 mb-7'>
-        <button className='border p-2 w-30 rounded-md'>View Projects</button>
-        <button className='border p-2 w-30 rounded-md'>Contact me</button>
+        <a 
+          className='border border-gray-300 p-2 w-30 rounded-md cursor-pointer'
+          href='#projects'>
+            View Projects
+        </a>
+        <a 
+          className='border border-gray-300 p-2 w-30 rounded-md'
+          href='#contact'>
+          Contact me
+        </a>
       </div>
-      <div className='flex gap-4'>
+      {/* <div className='flex gap-4'>
         <Github/>
         <Linkedin/>
         <Mail/>
-      </div>
+      </div> */}
      </section>
 
-     <section className='px-5 mb-15'>
+     <section className='px-5 mb-15'
+     id='skills'>
       <h2 className='font-heading text-2xl mb-5 text-center'>Skills and technologies</h2>
       <Skills
       title="Languages, Frameworks and Libraries"
@@ -77,45 +88,70 @@ function App() {
       />
      </section>
 
-     <section className='px-5 mb-15'>
+     <section className='px-5 mb-15'
+     id='projects'>
       <h2 className='font-heading text-2xl mb-5 text-center'>Projects</h2>
       <div className='flex flex-col gap-5 w-full items-center sm:flex-row sm:flex-wrap sm:justify-center'>
         <Projects
-        image={test1Img}
-        title="Project One"
-        description="A brief description of Project One."
+        image={animeProjectImg}
+        title="Anime Checklist"
+        description="A smart anime tracker powered by Gemini AI and Firebase"
         tools={['React','Tailwind CSS','Firebase']}
+        githubLink={'https://github.com/reyschwartz19/Anime-watchlist'}
+        demoLink={'https://anime-watchlist-one.vercel.app/#/auth'}
         />
-         <Projects
-        image={test1Img}
-        title="Project One"
-        description="A brief description of Project One."
-        tools={['React','Tailwind CSS','Firebase']}
-        />
+        
       
         
       </div>
      </section>
 
-     <section className='px-5'>
+     <section className='px-5'
+     id='contact'>
       <h2 className='font-heading text-2xl mb-5 text-center'>Contact Me</h2>
       <p className='text-center mb-5'>Looking for a frontend developer to bring your ideas to live? Reach out so we can turn those ideas to reality</p>
       <div className='flex flex-col w-full sm:flex-row sm:gap-4 sm:justify-center'>
-        <div className='flex flex-col items-center border border-gray-300 rounded-md p-5 mb-5 sm:w-50'>
-          <Mail className='mb-3 w-5'/>
-          <p className='mb-1 text-[0.8rem]'>Email</p>
-          <p className='text-black text-[0.8rem]'>njongremy30@gmail.com</p>
-        </div>
-        <div className='flex flex-col items-center border border-gray-300 rounded-md p-5 mb-5 sm:w-50'>
-          <Github className='mb-3 w-5'/>
-          <p className='mb-1 text-[0.8rem]'>Github</p>
-          <p className='text-black text-[0.8rem]'>njongremy30@gmail.com</p>
-        </div>
-        <div className='flex flex-col items-center border border-gray-300 rounded-md p-5 mb-5 sm:w-50'>
-          <Linkedin className='mb-3 w-5'/>
-          <p className='mb-1 text-[0.8rem]'>Linkedin</p>
-          <p className='text-black text-[0.8rem]'>njongremy30@gmail.com</p>
-        </div>
+       
+        <a 
+          href="mailto:njongremy30@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className='cursor-pointer'
+        >
+          <div className='flex flex-col items-center border border-gray-300 rounded-md p-5 mb-5 sm:w-50 sm:h-35'>
+            <Mail className='mb-3 w-5'/>
+            <p className='mb-1 text-[0.8rem]'>Email</p>
+            <p className='text-black text-[0.8rem]'>njongremy30@gmail.com</p>
+          </div>
+        </a>
+       
+        <a 
+          href="https://github.com/reyschwartz19"
+          target="_blank"
+          rel="noopener noreferrer"
+          className='cursor-pointer'
+        >
+          <div className='flex flex-col items-center border border-gray-300 rounded-md p-5 mb-5 sm:w-50 sm:h-35'>
+            <Github className='mb-3 w-5'/>
+            <p className='mb-1 text-[0.8rem]'>Github</p>
+            <p className='text-black text-[0.8rem]'>github.com/reyschwartz19</p>
+          </div>
+        </a>
+       
+        <a
+          href="https://www.linkedin.com/in/njong-remy-nga-i-bb7690343/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className='cursor-pointer'
+        >
+          <div className='flex flex-col items-center border border-gray-300 rounded-md p-5 mb-5 sm:w-50 sm:h-35'>
+            <Linkedin className='mb-3 w-5'/>
+            <p className='mb-1 text-[0.8rem]'>Linkedin</p>
+            <a className='text-black text-[0.8rem] text-center'>
+              www.linkedin.com/in/njong-remy-nga-i-bb7690343
+            </a>
+          </div>
+        </a>
       </div>
      </section>
 
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,8 +1,8 @@
 import { ExternalLink,Github } from "lucide-react";
 
-const Projects = ({image,title,description,tools}) =>{
+const Projects = ({image,title,description,tools,demoLink,githubLink}) =>{
    return(
-    <div className="border border-gray-300 rounded-md w-87.5">
+    <div className="border border-gray-300 rounded-md w-87.5 h-110">
        <img src={image} alt={title} className="w-full h-40 object-cover rounded-md mb-3"/>
        <div className="p-4">
            <h3 className="font-heading text-[1.2rem] mb-2">{title}</h3>
@@ -15,14 +15,22 @@ const Projects = ({image,title,description,tools}) =>{
             ))}
            </div>
            <div className="flex gap-4">
-            <button className="flex items-center p-2 rounded-md text-white bg-black">
+            <a 
+              href={demoLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center p-2 rounded-md text-white bg-black cursor-pointer">
                 <ExternalLink/>&nbsp;
                 Live Demo
-            </button>
-            <button className="flex items-center p-2 rounded-md text-black border border-gray-400">
+            </a>
+            <a 
+            href={githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center p-2 rounded-md text-black border border-gray-400 cursor-pointer">
                 <Github/>&nbsp; 
                 Code
-            </button>
+            </a>
            </div>
        </div>
     </div>
